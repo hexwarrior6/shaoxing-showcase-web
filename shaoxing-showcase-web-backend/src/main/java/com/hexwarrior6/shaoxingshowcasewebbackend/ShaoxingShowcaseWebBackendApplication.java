491fc0b2927093378a03e21bd2f26919988c5279
HexWarrior6 <3083512469@qq.com>
2024-12-22 21:27:34 +0800

    first admit

---
+++ shaoxing-showcase-web-backend/src/main/java/com/hexwarrior6/shaoxingshowcasewebbackend/ShaoxingShowcaseWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hexwarrior6.shaoxingshowcasewebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaoxingShowcaseWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaoxingShowcaseWebBackendApplication.class, args);
+    }
+
+}
---
+++ shaoxing-showcase-web-backend/src/test/java/com/hexwarrior6/shaoxingshowcasewebbackend/ShaoxingShowcaseWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexwarrior6.shaoxingshowcasewebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaoxingShowcaseWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
